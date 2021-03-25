04305d87f30e98452aef7920adc736865f07167d
zaracooper <zramcooper@gmail.com>
2021-03-25 08:46:56 +0300

    Add auth routes

---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+
+import { GrantTypes, isTokenCurrent } from '../helpers/token.js';
+import { makeAuthRequest } from '../helpers/request.js';
+
+var router = Router();
+
+router.get('/destroy', (req, res, next) => {
+    req.session.destroy(err => {
+        res.status(500).send({ error: err, message: 'Failed to log out' });
+        return;
+    });
+
+    res.status(200).send({ message: 'Successfully logged out' });
+});
+
+router.post('/token', (req, res, next) => {
+    const grantType = req.body.grantType;
+
+    function checkToken(token) {
+        if (isTokenCurrent(token)) {
+            res.send({ message: 'Issued access token is still valid' }).status(304);
+        } else {
+            makeAuthRequest(grantType, req, res);
+        }
+    }
+
+    switch (grantType) {
+        case GrantTypes.Password:
+            checkToken(req.session.customerToken);
+            break;
+        case GrantTypes.ClientCredentials:
+            checkToken(req.session.clientToken);
+            break;
+        default:
+            makeAuthRequest(grantType, req, res);
+    }
+});
+
+export default router;
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+var router = Router();
+
+export default router;
