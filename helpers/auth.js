e4776f7934f13092ce2a933407c33f2b853b7e91
zaracooper <zramcooper@gmail.com>
2021-03-31 18:09:50 +0300

    Remove unused auth middleware

--- helpers/auth.js
+++ helpers/auth.js
@@ -20,12 +20,4 @@ function checkAccessToken(req, res, next) {
     }
 }
 
-function checkCustomerToken(req, res, next) {
-    if (req.session.customerToken) {
-        next();
-    } else {
-        res.status(401).send({ message: 'Login required to access this route.' });
-    }
-}
-
-export { checkAccessToken, checkCustomerToken };
+export { checkAccessToken };
